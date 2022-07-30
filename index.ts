7c1756939e1da1d62fabe631f447a482151a5bc3
lusatiro <ljsatiro@gmail.com>
2022-07-30 14:12:54 -0300

    use fs promise

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import fsPromises from "fs/promises";
 import fs from "fs";
+
 import cors from "cors";
 
 const app = express();
@@ -12,18 +14,20 @@ app.get("/favicon.ico", (_, res) => {
 
 app.get("/:name", async (req, res) => {
   console.log(new Date().toISOString(), `/${req.params.name}`);
+  const files = await (await fsPromises.readdir(`${__dirname}/audios`));
+
+  res.setHeader('content-type', 'audio/wav')
+  const [ audioPath ] = files.filter(it => it.toLowerCase().includes(req.params.name.toLowerCase()))
+  
+  if (!audioPath) {
+    res.status(400)
+    return res.end()
+  }
+
   const readable = fs.createReadStream(
-    `${__dirname}/audios/${req.params.name}.wav`
+    `${__dirname}/audios/${audioPath}`
   );
-
-  await new Promise((resolve) => {
-    readable.on("data", (chunk) => {
-      console.log(chunk);
-      res.write(chunk);
-    });
-
-    readable.on("end", () => resolve(res.end()));
-  });
+  readable.pipe(res)
 });
 
 app.listen(8080, () => {
